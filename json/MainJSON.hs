5d4861b5904c4441dc83d8eb96fafffd97acfef8
kylobite <kylolink@gmail.com>
2014-03-07 19:18:54 -0600

    Added setup file and finished package

--- json/MainJSON.hs
+++ json/MainJSON.hs
@@ -5,3 +5,5 @@ import SimpleJSON
 main = print (JObject [("foo", JNumber 1),("bar", JBool False)])
 
 -- main acts like main() in Java and C
+
+-- Use Text.PrettyPrint.HughesPJ
---
+++ json/Setup.hs
@@ -0,0 +1,8 @@
+#!/usr/bin/env runhaskell
+import Distribution.Simple
+main = defaultMain
+
+{-|
+    Setup:  runghc Setup configure
+    Build:  runghc Setup build
+-}
